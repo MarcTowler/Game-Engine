fb9e8a42827b769a2e540a77052236fa5957e9e7
Marc Towler <marc@marctowler.co.uk>
2018-02-12 00:14:15 +0000

    Added gravity, jumping, collision detection, wall creation. Also moved classes to allow for inheritence rather then hard coding... Finally, I also remembered to add the images :P

--- GameEngine/Game1.cs
+++ GameEngine/Game1.cs
@@ -15,6 +15,9 @@ namespace GameEngine
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        public List<GameObject> objects = new List<GameObject>();
+        public Map map = new Map();
+
         public Game1() //This is the constructor, this function is called whenever the game class is created.
         {
             graphics = new GraphicsDeviceManager(this);
@@ -41,6 +44,9 @@ namespace GameEngine
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            map.Load(Content);
+            LoadLevel();
         }
 
         /// <summary>
@@ -50,6 +56,10 @@ namespace GameEngine
         /// </summary>
         protected override void Update(GameTime gameTime)
         {
+            Input.Update();
+
+            UpdateObjects();
+
             //Update the things FNA handles for us underneath the hood:
             base.Update(gameTime);
         }
@@ -62,8 +72,51 @@ namespace GameEngine
             //This will clear what's on the screen each frame, if we don't clear the screen will look like a mess:
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
+            spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.AlphaBlend);
+            DrawObjects();
+            map.DrawWalls(spriteBatch);        
+            spriteBatch.End();
+
             //Draw the things FNA handles for us underneath the hood:
             base.Draw(gameTime);
+        }
+
+        public void LoadLevel()
+        {
+            //Add the player
+            objects.Add(new Player(new Vector2(640, 360)));
+
+            //Add the walls
+            map.walls.Add(new Wall(new Rectangle(256, 256, 256, 256)));
+            map.walls.Add(new Wall(new Rectangle(0, 650, 1280, 128)));
+
+            LoadObjects();
+        }
+
+        public void LoadObjects()
+        {
+            for(int i = 0; i < objects.Count; i++)
+            {
+                objects[i].Initialize();
+                objects[i].Load(Content);
+            }
+        }
+
+        public void UpdateObjects()
+        {
+            for (int i = 0; i < objects.Count; i++)
+            {
+                objects[i].Update(objects, map);
+            }
+        }
+
+        public void DrawObjects()
+        {
+            for (int i = 0; i < objects.Count; i++)
+            {
+                objects[i].Draw(spriteBatch);
+            }
+
         }
     }
 }
---
+++ GameEngine/Player.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace GameEngine
+{
+    public class Player : Character
+    {
+        public Player()
+        {
+
+        }
+
+        public Player(Vector2 inputPosition)
+        {
+            position = inputPosition;
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public override void Load(ContentManager content)
+        {
+            image = TextureLoader.Load("sprite", content);
+
+            base.Load(content);
+        }
+
+        public override void Update(List<GameObject> objects, Map map)
+        {
+            CheckInput(objects, map);
+
+            base.Update(objects, map);
+        }
+
+        private void CheckInput(List<GameObject> objects, Map map)
+        {
+            if (Character.applyGravity == false)
+            {
+                if (Input.IsKeyDown(Keys.D) == true)
+                {
+                    MoveRight();
+                }
+                else if (Input.IsKeyDown(Keys.A) == true)
+                {
+                    MoveLeft();
+                }
+
+                if (Input.IsKeyDown(Keys.S) == true)
+                {
+                    MoveDown();
+                }
+                else if (Input.IsKeyDown(Keys.W) == true)
+                {
+                    MoveUp();
+                }
+            }
+            else
+            {
+                if (Input.IsKeyDown(Keys.D) == true)
+                {
+                    MoveRight();
+                }
+                else if (Input.IsKeyDown(Keys.A) == true)
+                {
+                    MoveLeft();
+                }
+
+                if(Input.KeyPressed(Keys.W) == true)
+                {
+                    Jump(map);
+                }
+            }
+        }
+    }
+}
